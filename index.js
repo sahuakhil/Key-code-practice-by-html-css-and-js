4a5a5bbe448e9b13f33983a64c78d1ed2afee658
Akhilesh <akhileshshah75@gmail.com>
2023-08-14 23:16:40 +0530

    Key code practice

---
+++ index.js
@@ -0,0 +1,14 @@
+let userInputEl = document.getElementById("userInput");
+let keyCodeListEl = document.getElementById("keyCodeList");
+
+function createAndAppendKeyCode(keyCode) {
+    let listItemEl = document.createElement("li");
+    listItemEl.classList.add("mt-1");
+    listItemEl.textContent = keyCode;
+    keyCodeListEl.appendChild(listItemEl);
+}
+
+function onkeydown(ever) {
+    createAndAppendKeyCode(event.keyCode);
+}
+userInputEl.addEventListener("keydown", keyCodeListEl);
